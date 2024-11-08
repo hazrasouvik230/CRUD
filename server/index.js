2a64b70ef447ab657b0067b6e1d0bbd4de817330
SOUVIK HAZRA <hazrasouvik230@gmail.com>
2024-11-08 20:01:33 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -1,85 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const UserModel = require("./models/Users");
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-
-// mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-// .then(() => {
-//     console.log(`MongoDB is connected successfully!`)
-// }).catch((error) => {
-//     console.error(`MongoDB connection error: ${error}`)
-// })
-
-// app.get("/", async (req, res) => {
-//     try {
-//         const users = await UserModel.find({});
-//         res.status(200).json(users);
-//     } catch (error) {
-//         res.status(500).json({error: "Failed to fetch users."})
-//     }
-// });
-
-// app.get("/getUser/:id", async (req, res) => {
-//     try {
-//         const user = await UserModel.findById(req.params.id);
-//         if(!user) {
-//             return res.status(404).json({error: "User not found"})   
-//         }
-//         res.status(200).json(user);
-//     } catch (error) {
-//         res.status(500).json({error: "Failed to fetch user."})
-//     }
-// })
-
-// app.put("/updateUser/:id", async (req, res) => {
-//     const {name, email, age} = req.body;
-//     try {
-//         const updateUser = await UserModel.findByIdAndUpdate(req.params.id, {name, email, age}, {new: true, runValidators: true});
-//         if(!updateUser) {
-//             return res.status(404).json({error: "User not found."})
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: "Failed to update user" });
-//     }
-// });
-
-// app.delete("/deleteUser/:id", async (req, res) => {
-//     try {
-//         const deleteUser = await UserModel.findByIdAndDelete(req.params.id);
-//         if(!deleteUser) {
-//             return res.status(404).json({ error: "User not found" });
-//         }
-//         res.status(200).json({ message: "User deleted successfully" });
-//     } catch (error) {
-//         res.status(500).json({ error: "Failed to delete user" });
-//     }
-// });
-
-// app.post("/createUser", async (req, res) => {
-//     try {
-//         const newUser = await UserModel.create(req.body);
-//         res.status(201).json(newUser);
-//     } catch (error) {
-//         res.status(500).json({ error: "Failed to create user" });
-//     }
-// });
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server is running on http://localhost:${process.env.PORT}`);
-// })
-
-
-
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -89,7 +7,11 @@ const UserModel = require("./models/Users");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ["http://crud-orcin-one.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 
